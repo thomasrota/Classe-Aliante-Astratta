1f12315b24e8ce8d74b4a1ec813dcf804461902c
Thomas Rota <rota.thomas.studente@itispaleocapa.it>
2024-01-06 11:08:21 +0100

    <creazione classe Composite>

---
+++ Classe Aliante Astratta/Composite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Aliante_Astratta
+{
+	public abstract class Composite
+	{
+		public abstract void Aggiunta(Composite componente);
+
+		public abstract void Rimuovi(int indice);
+
+		public abstract Composite GetChild(int indice);
+
+		public new abstract bool Equals(object obj);
+
+		public new abstract string ToString();
+
+		public abstract double Prezzo();
+	}
+}
